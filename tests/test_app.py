68019e4e882ba7105df4f9eec0f25520b04abb91
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2024-07-20 23:52:08 -0300

    Project setup

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+
+def test_read_root_should_return_ok_and_hello_world():
+    client = TestClient(app)
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello, World'}
