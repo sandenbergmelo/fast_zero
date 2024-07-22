bd10f46865280d02c960c987e8df051b11982488
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2024-07-22 16:28:33 -0300

    Create users table using SQLAlchemy and add first migration with alembic

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,9 @@
 from http import HTTPStatus
 
+from fastapi.testclient import TestClient
 
-def test_read_root_should_return_ok_and_hello_world(client):
+
+def test_read_root_should_return_ok_and_hello_world(client: TestClient):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
---
+++ tests/test_db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from fastapi_zero.db.models import User
+
+
+def test_create_user(session: Session):
+    user = User(username='Hello', email='hello@world.com', password='secret')
+
+    session.add(user)
+    session.commit()
+
+    result = session.scalar(
+        select(User).where(User.email == 'hello@world.com')
+    )
+
+    assert user.username == result.username
