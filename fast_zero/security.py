d719609aa45f83dc3f8562b7f6eb22b0fc63003a
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2024-07-25 18:28:54 -0300

    Change all HTTPStatus to FastAPI status

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
-from http import HTTPStatus
 
 import jwt
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jwt.exceptions import PyJWTError
 from pwdlib import PasswordHash
@@ -49,7 +48,7 @@ def get_current_user(
     token: str = Depends(oauth2_scheme),
 ):
     credentials_exception = HTTPException(
-        status_code=HTTPStatus.UNAUTHORIZED,
+        status_code=status.HTTP_401_UNAUTHORIZED,
         detail='Could not validate credentials',
         headers={'WWW-Authenticate': 'Bearer'},
     )
@@ -68,7 +67,7 @@ def get_current_user(
 
     if not user:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=status.HTTP_404_NOT_FOUND, detail='User not found'
         )
 
     return user
