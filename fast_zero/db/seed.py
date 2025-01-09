789f6f3f975a1c059c201f44a0c6c1ae6766f82f
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2025-01-09 08:22:02 -0300

    Add seed script

---
+++ fast_zero/db/seed.py
@@ -0,0 +1,73 @@
+import sys
+from pathlib import Path
+
+import alembic
+import alembic.command
+from alembic.config import Config
+from rich import print
+from sqlalchemy import text
+
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
+import factory
+import factory.fuzzy
+
+from fast_zero.db.connection import get_session
+from fast_zero.db.models import Todo, TodoState, User
+from fast_zero.helpers.security import get_password_hash
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'test{n}')
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
+    password = factory.LazyAttribute(
+        lambda obj: get_password_hash(f'{obj.username}+password')
+    )
+
+
+class TodoFactory(factory.Factory):
+    class Meta:
+        model = Todo
+
+    title = factory.Faker('text')
+    description = factory.Faker('text', max_nb_chars=50)
+    state = factory.fuzzy.FuzzyChoice(TodoState)
+    user_id = factory.fuzzy.FuzzyInteger(1, 5)
+
+
+alembic_config = Config(Path(__file__).parent.parent.parent / 'alembic.ini')
+session = next(get_session())
+
+# Delete all tables
+print('[bold yellow]Dropping database...[/]')
+alembic.command.downgrade(alembic_config, 'base')
+session.execute(text('DROP TYPE IF EXISTS todostate'))
+session.commit()
+print('[bold green]Database dropped![/]')
+
+# Create all tables
+print('[bold yellow]Creating database...[/]')
+alembic.command.upgrade(alembic_config, 'head')
+print('[bold green]Database created![/]')
+
+users = UserFactory.create_batch(5)
+todos = TodoFactory.create_batch(10)
+
+users.insert(
+    0,
+    User(
+        username='admin',
+        password=get_password_hash('admin'),
+        email='admin@admin.com',
+    ),
+)
+
+print('[bold yellow]Seeding database...[/]')
+session.add_all(users)
+session.commit()
+session.add_all(todos)
+session.commit()
+print('[bold green]Database seeded![/]')
